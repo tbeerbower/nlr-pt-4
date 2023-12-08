a7a0e99cbaa291a5d619a1ed38453d978f73241e
Tom Beerbower <thomas@techelevator.com>
2023-12-07 20:14:20 -0500

    add name

--- js/green-scripts.js
+++ js/green-scripts.js
@@ -10,6 +10,7 @@ classInfo.push({ name: 'Ian bird', fanPageUrl: 'https://birdmandeveloper.github.
 classInfo.push({ name: 'David Smith', fanPageUrl: 'https://dtsmth.github.io/fan/' });
 classInfo.push({ name: 'Matthew Gillott', fanPageUrl: 'https://mgillott.github.io/fan/' });
 classInfo.push({ name: 'Trevor Kline', fanPageUrl: 'https://tkline12.github.io/fan/' });
+classInfo.push({ name: 'Adrian Carlisle Jr', fanPageUrl: 'https://dawgzz.github.io/Fan' });
 
 
 window.addEventListener('DOMContentLoaded', event => {
